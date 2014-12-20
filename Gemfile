32350e710b85b6e4a5efcecd38061b3d54c7e52d
Conroy Whitney <conroy.whitney@gmail.com>
2014-12-20 00:03:41 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+ruby "2.0.0"
+
+# server
+gem 'thin'
+
+# rails
+gem 'rails', '~> 3'
+
+# heroku
+gem 'rails_12factor'
+
+# db
+gem 'pg'
+
+# search
+gem 'algoliasearch-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apprenticeships::Application.config.secret_token = '1df6702bc44b6984fdcdcd39f659ee2361d9c02d1033f13c62353adfc2428e2cf2d3ebfdb15d6bb43ab6004678c2dd6a8b4eb6c9659624ff1cec43d0d972eb1b'
